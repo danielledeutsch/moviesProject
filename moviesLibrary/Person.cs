7b4e34058e822c5323c7e3de3141465a938830c4
danielledeutsch <131603791+danielledeutsch@users.noreply.github.com>
2025-01-30 14:04:41 +0200

    Add files via upload

---
+++ moviesLibrary/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moviesLibrary
+{
+
+    public enum UserRole
+    {
+        RegularUser = 0,
+        Admin = 1
+    }
+    // Base class for shared properties
+    public class Person
+    {
+        public int Id { get; set; } // Use Id to generalize UserId and ActorId
+        public string? Name { get; set; }
+    }
+
+    public class User : Person
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public class Actor : Person
+    {
+        public string? PictureURL { get; set; }
+    }
+
+
+}
---
+++ moviesLibrary/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moviesLibrary
+{
+    public class UserLoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserRegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
